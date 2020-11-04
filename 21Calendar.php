5a9b9e83c70d7f63784a38bfd95f807959173d0d
Kyosukewu <71436840+Kyosukewu@users.noreply.github.com>
2020-11-04 20:06:21 +0800

    部分內容微調

--- 21Calendar.php
+++ 21Calendar.php
@@ -5,11 +5,239 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/style.css">
+  <!-- <link rel="stylesheet" href="css/style.css"> -->
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
   <script src="https://kit.fontawesome.com/a1381bb91e.js" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <title>Kyosuke's Calendar</title>
 </head>
+<style>
+  body {
+    height: 100vh;
+    background-image: linear-gradient(135deg, #fff 30%, #eee 80%);
+  }
+
+  .card {
+    box-shadow: 0px 0px 15px #555;
+  }
+
+  .item {
+    position: relative;
+  }
+
+  .overlay {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    background: rgba(0, 0, 0, 0.4);
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    color: #fff;
+  }
+
+  table {
+    height: 80vh;
+  }
+
+  thead tr {
+    background: #333;
+    height: 50px;
+    color: #fff;
+  }
+
+  tbody tr {
+    background: #ddd;
+  }
+
+  tbody td {
+    font-size: 20px;
+    height: 12%;
+    color: #666;
+  }
+
+  tbody td:nth-child(6n+1) {
+    background: #fcc;
+    color: #666;
+  }
+
+  thead td:nth-child(6n+1) {
+    background: rgb(131, 18, 18);
+  }
+
+  .today {
+    color: #fff;
+    background: rgb(124, 246, 255);
+    box-shadow: 0 0 20px #fff;
+    text-shadow: 0 0 10px rgb(50, 111, 116);
+    /* opacity: .3; */
+  }
+
+  .pmonth,
+  .nmonth {
+    background: #000;
+    color: #fff;
+    opacity: .1;
+  }
+
+  .pick {
+    width: 150px;
+  }
+
+  @media screen and (min-width: 1500px) {
+    .alert {
+      top: 3%;
+      right: 35%;
+      opacity: .9;
+      z-index: 10;
+    }
+
+    .btn2 {
+      display: none;
+    }
+
+    .carousel-control-prev {
+      left: -5%;
+      width: 5%;
+      flex-direction: column;
+    }
+
+    .carousel-control-next {
+      right: -5%;
+      width: 5%;
+      flex-direction: column;
+    }
+
+    .btn2-p {
+      left: -8%;
+      width: 3%;
+    }
+
+    .btn2-n {
+      right: -8%;
+      width: 3%;
+    }
+  }
+
+  @media screen and (max-width: 1500px) and (min-width: 768px) {
+    .alert {
+      top: 3%;
+      right: 10%;
+      opacity: .9;
+      z-index: 10;
+    }
+
+    .btn2 {
+      display: none;
+    }
+
+    .card-body {
+      position: relative;
+    }
+
+    .carousel-control-prev {
+      left: 20%;
+      width: 5%;
+      top: 95%;
+      height: 5%;
+    }
+
+    .carousel-control-next {
+      right: 20%;
+      width: 5%;
+      top: 95%;
+      height: 5%;
+    }
+
+    .d1 {
+      margin-left: .5rem;
+      margin-bottom: .5rem;
+    }
+
+    .d2 {
+      margin-right: .5rem;
+      margin-bottom: .5rem;
+      order: -1;
+    }
+
+    .btn2-p {
+      left: 5%;
+      width: 3%;
+    }
+
+    .btn2-n {
+      right: 5%;
+      width: 3%;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    .alert {
+      top: 3%;
+      right: 0%;
+      opacity: .9;
+      z-index: 10;
+    }
+
+    table {
+      height: 60vh;
+    }
+
+    .btn1 {
+      display: none;
+    }
+
+    .card-body {
+      position: relative;
+    }
+
+    .carousel-control-prev {
+      left: 15%;
+      width: 5%;
+      justify-content: center;
+    }
+
+    .carousel-control-next {
+      right: 15%;
+      width: 5%;
+      justify-content: center;
+    }
+
+    .btn2-p {
+      left: 5%;
+      width: 3%;
+    }
+
+    .btn2-n {
+      right: 5%;
+      width: 3%;
+    }
+  }
+
+  @media screen and (max-height: 700px) {
+
+    .d1,
+    .d2 {
+      display: none;
+    }
+
+    .carousel-control-prev {
+      width: 5%;
+      bottom: 1%;
+      justify-content: flex-end;
+    }
+
+    .carousel-control-next {
+      width: 5%;
+      bottom: 1%;
+      justify-content: flex-end;
+    }
+  }
+</style>
 
 <body>
   <?php
@@ -18,9 +246,6 @@
   //date_default_timezone_set("Asia/Taipei");
   $year = date('Y');
   $month = date('m');
-  $rYear = rand(date('Y'), date('Y') + 10);
-  $rMonth = rand(1, 12);
-  $rDay = rand(1, date('t', $rMonth));
   ?>
   <div class="container-xxl">
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
@@ -45,34 +270,40 @@
           $today = date('d', $fDate); //今日日期
           $pDays = date('t', strtotime("{$year}-{$thisMonth}-1 -1 Months")); //上月天數
           $nDays = date('w', strtotime("{$year}-{$thisMonth}-{$monthDay}")); //本月結束是周幾
-          ?>
+          $rYear = rand(date('Y'), date('Y'));
+          $rMonth = rand(date('m'), 12);
+          $rDay = rand($today, date('t', $rMonth));
 
+          ?>
           <form class="d-flex" action="21Calendar.php" method="get">
-            <input class="form-control mr-2" type="text" name="year" maxlength="4" size="3" placeholder="YYYY" value="<?=date('Y')?>" required>
+            <input class="form-control mr-2" type="text" name="year" maxlength="4" size="3" placeholder="YYYY" value="<?= date('Y') ?>" required>
 
             <select class="form-control mr-2" name="month">
               <?php
               for ($i = 1; $i < 13; $i++) {
-                if($i==$thisMonth){
+                if ($i == $thisMonth) {
                   echo "<option value='" . $i . "' selected>" . $i . "月</option>";
-                }else{
+                } else {
                   echo "<option value='" . $i . "'>" . $i . "月</option>";
                 }
               }
               ?>
             </select>
             <button class="btn btn-outline-secondary" type="submit" value="submit">Search</button>
-            <!-- <div class="w-25">
-          <a class="btn btn-outline-secondary" href="21Calendar.php?year=<?php echo $rYear ?>&month=<?php echo $rMonth ?>">Pick good day!</a>
-      <div class="alert alert-warning" role="alert">
-        <?= "Info：幫您精挑細選了" . $year . "年" . $thisMonth . "月" . $rDay . "日這個特別日子，趕快安排些活動吧！" ?>
-        </div>
-        </div> -->
+            <div id="pick">
+              <a class="pick btn btn-outline-secondary ml-3" href="21Calendar.php?year=<?php echo $year ?>&month=<?php echo $rMonth ?>">Pick good day!</a>
+            </div>
+            <!-- <script type="text/javascript">
+              $(document).ready(function() {//文件準備完畢開始做事
+                //做事的位置
+                $("#pick").click(function() {
+                  $("#alert").show();
+                })
+              })
+            </script> -->
           </form>
         </div>
       </div>
-
-
     </nav>
     <?php
     //定義一個月有幾週
@@ -116,6 +347,10 @@
     ];
     $ec = $enmonth[$thisMonth];
     ?>
+    <div id="alert" class="alert position-absolute alert-warning alert-dismissible fade show" role="alert">
+      <?= "Info：幫您精挑細選了" . $year . "年" . $thisMonth . "月" . $rDay . "日這個特別日子，趕快安排些活動吧！" ?>
+      <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+    </div>
     <div class="card mb-3 vh-75">
       <div class="row g-0">
         <div class="item col-md-3 border-right">
